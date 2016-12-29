0483f8555fd39eb814ee15be5e2fa7c23113a80c
Alexey Maslov <lelick29@gmail.com>
2016-12-29 23:12:28 +0300

    parser logs

--- src/app/app.php
+++ src/app/app.php
@@ -10,15 +10,20 @@ namespace app;
 use anmaslov\parser\ItemBuilder;
 use anmaslov\parser\BuilderPriceRu;
 
+use Monolog\Logger;
+use Monolog\Handler\StreamHandler;
+
 define('MODX_API_MODE', true);
 
 
 class App
 {
     public $modx;
+    private $log;
     private $username = 'nortel';
     private $password = 'password';
-    private $dirImage = "/upload/img/parser/";                                          // путь к фото
+    private $dirImage = "/upload/img/parser/";
+    private $parsed = 0; //count of parsed items
     private $parent = 18491; //Родительская категория
 
     public function init()
@@ -27,6 +32,9 @@ class App
         require_once MODX_BASE_PATH . 'index.php';
 
         $this->modx = $modx;
+
+        $this->log = new Logger('name');
+        $this->log->pushHandler(new StreamHandler(MODX_BASE_PATH . 'parser/log/' . date("Y_m_d_H") . '.log', Logger::DEBUG));
     }
 
     public function winToUtf($str){
@@ -35,19 +43,21 @@ class App
 
     public function run()
     {
-
         $this->init();
+        $this->log->debug('init system');
         //Понеслось говнище по трубам
         // Load main services
         $this->modx->setLogTarget(XPDO_CLI_MODE ? 'ECHO' : 'HTML');
         $this->modx->getService('error','error.modError');
         $this->modx->lexicon->load('minishop2:default');
         $this->modx->lexicon->load('minishop2:manager');
+        $this->log->debug('load modx modules');
 
         // Логинимся в админку
         $response = $this->modx->runProcessor('security/login', array('username' => $this->username, 'password' => $this->password));
         if ($response->isError()) {
             $this->modx->log(modX::LOG_LEVEL_ERROR, $response->getMessage());
+            $this->log->warning('error auth', $response->getMessage());
             return;
         }
         $this->modx->initialize('mgr');
@@ -72,38 +82,48 @@ class App
                 //'sortdir' => 'asc',
                 )
         );
-        echo "<pre>".date("d.m.Y - H:i:s") . "\r\nВсего пустых записей: ".count($arItems) . "\r\n <br />";
-        print_r($this->modx->getPlaceholder('pdoTools.log'));
-        //print_r($arItems);
-        //cnt = 95
-        echo '</pre>';
+        $this->log->info('load empty items '. count($arItems));
+        //$this->log->debug($this->modx->getPlaceholder('pdoTools.log'));
 
         $i = 0;
         foreach ($arItems as $arItem)
         {
+            $this->log->debug("Parse" . ++$i . "of " . count($arItems));
+
             if (trim($arItem['pagetitle']) != '') {
                 $this->makeRow($arItem);
             }
-            /*//tmp for test
-            if (++$i > 5){
+            //tmp for test
+            //if ($i > 2)
+            {
                 break;
-            }*/
+            }
         }
+
+        $this->log->info("Parse complete, parsed {$this->parsed}, items to parse (" . count($arItems) .")");
     }
 
-    private function makeRow($arItem) {
-       
+    private function makeRow($arItem)
+    {
         $itemBuilder = new ItemBuilder(new BuilderPriceRu);
-        $itemBuilder->constructItem($arItem['longtitle']);
+        //$itemBuilder->constructItem($arItem['longtitle']);
+        $itemBuilder->constructItem('Samsung J120F Galaxy J1 8Gb Black');
+        $this->log->debug('start parse from remote host');
         $shopItem = $itemBuilder->getItem();
+        //$this->log->debug('parse complite', $shopItem);
 
         if (count($shopItem->getProperies()) > 0) {
             $res = $this->modx->getObject('msProduct',array('id' => $arItem['id']));
             $res->set('content', $shopItem->getPropTable());
             if ($res->save()) {
                 $this->modx->cacheManager->clearCache(); //если сохранение успешно, то чистим кэш
-                echo "ok, id ". $arItem['id'] ." was updated<br>";
+                $this->log->debug("ok, id ". $arItem['id'] ." was updated");
+                $this->parsed++;
+            }else{
+                $this->log->error("error when trying save, id ". $arItem['id'], $shopItem->getProperies());
             }
+        }else{
+            $this->log->info("Item no parsed", $arItem);
         }
 
         $arImgs = array();
@@ -111,13 +131,14 @@ class App
             $p =  strrpos($arImg['src'], '/');
             $arImgs[] = substr($arImg['src'], $p+1);
         }
+        $this->log->debug("Cnt of items image gallery " . count($arImgs), $arImgs);
 
         if (count($arImgs)) {
             //import img
             foreach ($arImgs as $val) {
                 $image = str_replace('//', '/', MODX_BASE_PATH . $this->dirImage. $val);
                 if (!file_exists($image)) {
-                    echo "Could not import image $val to gallery. File $image not found on server.<br>";
+                    $this->log->error("Could not import image $val to gallery. File $image not found on server");
                 }
                 else {
                     $res = $this->modx->runProcessor('gallery/upload',
@@ -125,15 +146,14 @@ class App
                         array('processors_path' => MODX_CORE_PATH.'components/minishop2/processors/mgr/')
                     );
                     if ($res->isError()) {
-                        print_r($res->getAllErrors(), 1);
+                        $this->log->error("Error when trying save image to gallery", $res->getAllErrors());
                     }else{
-                        echo "Item with id ".$arItem['id']." img update <br>";
+                        $this->log->info("Item with id ".$arItem['id']." img update");
                     }
                     unset($res);
                 }
             }
         }
-
         unset($itemBuilder);
     }
 }
